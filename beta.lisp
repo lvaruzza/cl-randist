a43a6cad427921f4414139ff5aeea8ac436cd180
AJ Rossini <blindglobe@gmail.com>
2009-05-08 18:11:07 +0200

    include density-beta, using formula in doc-string for random-beta.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- beta.lisp
+++ beta.lisp
@@ -27,3 +27,12 @@ The method used here is the one described in Knuth "
 	(x2 (random-gamma b 1d0)))
     (declare (double-float x1 x2))
     (/ x1 (+ x1 x2))))
+
+
+(defun density-beta (x &key (a 1.0d0) (b 1.0d0))
+  "implemented according to docstring in random-beta."
+  (* (/ (gamma-function (+ a b))
+	(*  (gamma-function a)
+	    (gamma-function b)))
+     (^ x (- a 1))
+     (^ (- 1 x) (- b 1))))
