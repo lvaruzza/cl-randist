0904584088b3d62b28bf1ccc639fe83a7f40337d
Robert P. Goldman <rpgoldman@gmail.com>
2013-06-20 17:03:18 -0500

    Added IGNORE declaration.

--- tests.lisp
+++ tests.lisp
@@ -21,24 +21,25 @@
 (defun test-dist (f &optional (n 1000))
   (let ((lst (genlist f n)))
     (values (float (mean lst))
-	    (float (var lst)))))
+            (float (var lst)))))
 
 (defvar *n* (* 100 1000 1000))
 (defun test-gamma-speed (&optional (n *n*))
   (time (dotimes (i n)
-	  (random-gamma1 2.4d0 0.5d0))))
+          (random-gamma1 2.4d0 0.5d0))))
 
 (defun test-gamma-mt-speed (&optional (n *n*))
   (time (dotimes (i n)
-	  (random-gamma-mt 2.4d0 0.5d0))))
+          (random-gamma-mt 2.4d0 0.5d0))))
 
 (defun test-zigg-speed (&optional (n *n*))
   (time (dotimes (i n)
-	  (random-normal-ziggurat 0d0 1d0))))
+          (random-normal-ziggurat 0d0 1d0))))
 
 (defun test-binomial (&optional (n *n*))
   (test-dist #'(lambda (x)
-		 (random-binomial 0.3d0 10)) n))
+                 (declare (ignore x))
+                 (random-binomial 0.3d0 10)) n))
 
 #+sbcl
 (defun profile-gamma (&optional (n (* 100 1000)))
